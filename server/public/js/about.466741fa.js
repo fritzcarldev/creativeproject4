3d05fb8351a2d4aedcaf45d9cb3db75c2355284f
Fritz-Carl Morlant <prince.fritzcarl@gmail.com>
2022-04-03 22:29:08 -0600

    Second try with build

--- server/public/js/about.466741fa.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self["webpackChunkclient"]=self["webpackChunkclient"]||[]).push([[443],{830:function(e,t,n){n.r(t),n.d(t,{default:function(){return r}});var u=function(){var e=this,t=e.$createElement;e._self._c;return e._m(0)},l=[function(){var e=this,t=e.$createElement,n=e._self._c||t;return n("div",{staticClass:"about"},[n("h1",[e._v("This is an about page")])])}],s=n(1),a={},c=(0,s.Z)(a,u,l,!1,null,null,null),r=c.exports}}]);
-//# sourceMappingURL=about.466741fa.js.map
