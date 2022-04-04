6f498cc52002099fbfb13432cd6a9a27f88f6376
Fritz-Carl Morlant <prince.fritzcarl@gmail.com>
2022-04-03 22:21:24 -0600

    First commit

---
+++ server/index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+
+// Middleware
+
+app.use(bodyParser.json());
+app.use(cors());
+
+const posts = require("./routes/api/posts");
+
+app.use("/api/posts", posts);
+
+// Handle production
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(__dirname + "/public/"));
+
+  // Handle SPA
+  app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
+}
+
+//could use cors if needed
+
+const port = process.env.port || 3000;
+
+app.listen(port, () => console.log(`Server Start on port ${port}`));
